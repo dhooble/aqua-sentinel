b9a6c471fdd0d50f5bade26ec7a9cf5ce3d6bd85
Josselin SIMION <josselin.simion@univ-tlse3.fr>
2023-04-14 18:16:51 +0200

    feat(ds3231): adds the support for the RTC DS3231

---
+++ rtc_ds3231.h
@@ -0,0 +1,33 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+#ifndef RTC_DS3231_H
+#define RTC_DS3231_H
+
+#include "project.h"
+
+#define RTC_DS3231_I2C_ADDR 0b1101000
+
+
+
+
+
+
+
+
+
+
+#endif
+
+//1101000
+
+/* [] END OF FILE */
