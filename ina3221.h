b94d312c4f4601b1cb94a4b7a4edbc7c18f53eb5
Josselin SIMION <josselin.simion@univ-tlse3.fr>
2023-04-14 18:11:44 +0200

    feat(ina3221): adds support for the INA3221 voltage monitor

---
+++ ina3221.h
@@ -0,0 +1,35 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+
+
+
+#ifndef INA3221_H
+#define INA3221_H
+
+#include "project.h"
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
+
+
+/* [] END OF FILE */
